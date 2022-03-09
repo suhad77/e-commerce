7712bb8fa41153964a8dea0f4b44b0219446fd9b
user <86485005+Suhad77@users.noreply.github.com>
2022-03-09 11:18:17 +0600

    class test

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../compornents/partion/Header'
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+    </>
+  )
+}
+
+export default Home
