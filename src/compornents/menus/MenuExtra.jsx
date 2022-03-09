4ac2b31336e0da38d4038690acd4cf7ed57a8741
user <86485005+Suhad77@users.noreply.github.com>
2022-03-09 11:20:57 +0600

    class test

---
+++ src/compornents/menus/MenuExtra.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MenuExtra = () => {
+  return (
+    <div>MenuExtra</div>
+  )
+}
+
+export default MenuExtra
---
+++ src/compornents/menus/index.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+
+const Menus = () => {
+  return (
+    <div>Menus</div>
+  )
+}
+
+export default Menus
