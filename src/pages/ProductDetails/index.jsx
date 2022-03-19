0065b915409392016c0539abc135adadc9ddfb59
Suhad Mia <suhadmia12@gmail.com>
2022-03-19 21:03:15 +0600

    some design add

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import Footer from '../../compornent/partial/footer/Footer'
 import Header from '../../compornent/partial/Header'
 import Hero from '../../compornent/partial/productDetails/Hero'
+import ProductDetails from '../../compornent/partial/productDetails/ProductDetails'
 
 const ProductDeatils = () => {
   return (
     <div>
         <Header />
         <Hero />
+        <ProductDetails />
         <Footer />
     </div>
   )
 }
-
 export default ProductDeatils
