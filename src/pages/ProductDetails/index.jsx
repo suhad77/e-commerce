402ef35e383fd7b60af66c72e16e4352d430b46d
Suhad Mia <suhadmia12@gmail.com>
2022-03-21 18:02:30 +0600

    some design add

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../../compornent/partial/footer/Footer'
 import Header from '../../compornent/partial/Header'
 import Hero from '../../compornent/partial/productDetails/Hero'
+import PproductTab from '../../compornent/partial/productDetails/PproductTab'
 import ProductDetails from '../../compornent/partial/productDetails/ProductDetails'
 
 const ProductDeatils = () => {
@@ -10,6 +11,7 @@ const ProductDeatils = () => {
         <Header />
         <Hero />
         <ProductDetails />
+        <PproductTab />
         <Footer />
     </div>
   )
